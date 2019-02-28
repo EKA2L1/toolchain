62a1c209853070630e8a0eac320827e40770cf3d
pent0 <fewdspuckrito@gmail.com>
2019-03-01 06:52:15 +0700

    libraries: Set global destructor TLS if e32_run_thread is called for init process

--- libraries/src/entries/exethread.c
+++ libraries/src/entries/exethread.c
@@ -1,6 +1,6 @@
 #include <epoc/common.h>
 
-#ifdef __GCC32__
+#ifdef __GNUC__
 typedef void (*PFV)();
 extern PFV __CTOR_LIST__[];
 extern PFV __DTOR_LIST__[];
@@ -22,10 +22,14 @@ void e32_global_constructor_func() {
 }
 #endif
 
+#define E32_GLOBAL_DESTRUCTOR_KEY 0x80001010
+#define E32_SPECIAL_DLL_UID       -1
+
 void e32_run_thread(bool is_not_first_thread_created, void *thread_info)
 {
     if (!is_not_first_thread_created)
     {
         // This is the first thread in the process, setup some stuffs
+        int32 err = e32_set_dll_tls(E32_GLOBAL_DESTRUCTOR_KEY, E32_SPECIAL_DLL_UID, (void*)e32_global_constructor_func);
     }
 }
